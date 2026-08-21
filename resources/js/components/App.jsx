df0aa08548e008642b58eb471844818cdba95bad
antonkluge202 <anton@hrsignal.com>
2026-08-21 10:53:05 +0300

    moved the sumary tiles to the right of the filters component

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -6,10 +6,14 @@ export default function App() {
     return (
         <div className="mx-auto max-w-5xl p-8">
             <h1 className="text-2xl font-semibold text-gray-900">Transactions Dashboard</h1>
-            <div className="mt-6 flex flex-col gap-6">
-                <SummaryTiles className="max-w-2xl" />
-                <FiltersPanel className="max-w-sm" />
-                <CategoryBreakdown className="max-w-sm" />
+            <div className="mt-6 grid items-start gap-6 lg:grid-cols-[18rem_minmax(0,1fr)]">
+                <div className="flex flex-col gap-6">
+                    <FiltersPanel />
+                    <CategoryBreakdown />
+                </div>
+                <div className="flex flex-col gap-6">
+                    <SummaryTiles />
+                </div>
             </div>
         </div>
     );
